e9ce970cf2ce8ba51e57167938ae9dfc2ecb909f
yunbom-sudo <yunbom100929@gmail.com>
2026-08-11 11:16:36 +0900

    change request DTO constructor access level to PROTECTED

--- src/main/java/com/yunbom/homework/auth/dto/request/SignupRequest.java
+++ src/main/java/com/yunbom/homework/auth/dto/request/SignupRequest.java
@@ -1,10 +1,11 @@
 package com.yunbom.homework.auth.dto.request;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Setter
 public class SignupRequest {
