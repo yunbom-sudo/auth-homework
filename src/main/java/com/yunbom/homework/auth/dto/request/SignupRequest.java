bba613288ce8132bfdf4276b27248b635641deb1
yunbom-sudo <yunbom100929@gmail.com>
2026-08-13 14:46:03 +0900

    fix: SignupRequest

--- src/main/java/com/yunbom/homework/auth/dto/request/SignupRequest.java
+++ src/main/java/com/yunbom/homework/auth/dto/request/SignupRequest.java
@@ -15,7 +15,6 @@ public class SignupRequest {
     @Email(message = "이메일 형식이 옳바르지 않습니다.")
     private String email;
 
-    @Email(message = "비밀번호를 입력하세요.")
     @Pattern(
             regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$",
             message = "비밀번호는 8자 이상이며, 영문, 숫자, 특수문자를 모두 포함해야 합니다."
